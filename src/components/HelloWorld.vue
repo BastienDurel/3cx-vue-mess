4f14a505f9f642556d067a6d3918f8fd7a782312
Bastien Durel <bastien.durel@data.fr>
2023-12-21 11:34:40 +0100

    reproduce

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,65 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { Grid } from '@progress/kendo-vue-grid'
+import { Button, ButtonGroup } from "@progress/kendo-vue-buttons"
+import converter from "number-to-words"
 
-defineProps<{ msg: string }>()
+const COLUMNS = [
+  { field: "name", title: "Name" },
+  { title: "Optional 1", id: "o1", cell: "empty" },
+  { title: "Optional 2", id: "o2", cell: "randomString" },
+  { field: "number", title: "Number" },
+  { title: "Optional 3", id: "o3", cell: "randomString" },
+  { title: "empty", cell: "empty" },
+  { field: "number", title: "Number 2" },
+  { title: "Optional 4", id: "o4", field: "number2" },
+  { field: "number", title: "Number 3" },
+  { title: "Optional 5", id: "o5", cell: "randomString" },
+];
 
-const count = ref(0)
+const data = ref([gen(1), gen(2), gen(125478541), getRandom(), getRandom()])
+const columns = ref(COLUMNS);
+
+function gen(num: number): any {
+  return { number: num, name: converter.toWords(num), number2: Math.ceil(Math.random() * 100) }
+}
+function getRandom(): any {
+  return gen(100000000 + Math.ceil(Math.random() * 100000000))
+}
+function hide(col: string) {
+  columns.value!.filter(x => x.id == col).forEach((x: any) => x.hidden = !x.hidden);
+}
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <ButtonGroup>
+    <Button @click="() => data.push(getRandom())">Add</Button>
+    <Button @click="() => data.push(gen(Math.ceil(Math.random() * 100)))">Add Small</Button>
+  </ButtonGroup>
+  <ButtonGroup>
+    <Button @click="() => hide('o1')">Toggle optional 1</Button>
+    <Button @click="() => hide('o2')">Toggle optional 2</Button>
+    <Button @click="() => hide('o3')">Toggle optional 3</Button>
+    <Button @click="() => hide('o4')">Toggle optional 4</Button>
+    <Button @click="() => hide('o5')">Toggle optional 5</Button>
+  </ButtonGroup>
+  <Grid :data-items="data" :columns="columns">
+    <template v-slot:empty>
+      <td></td>
+    </template>
+    <template v-slot:randomString="{ props }">
+      <td>{{ props.dataItem.randomString || 'some-string' }}</td>
+    </template>
+  </Grid>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+.k-button-group {
+  margin-bottom: 1em;
+  margin-left: 1em;
+}
+
+.k-button-group:first-child {
+  margin-left: 0;
 }
 </style>
